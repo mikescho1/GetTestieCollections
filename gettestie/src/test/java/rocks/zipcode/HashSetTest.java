a40e014c0c77ae5856a0c6f6ce7a3dc18e1a0ce1
mike <mike.schoenbeck@gmail.com>
2019-11-03 19:26:36 -0500

    finished hashset tests

--- gettestie/src/test/java/rocks/zipcode/HashSetTest.java
+++ gettestie/src/test/java/rocks/zipcode/HashSetTest.java
@@ -23,18 +23,45 @@ public class HashSetTest {
 
         ArrayList<Person> personArrayList = new ArrayList<>();
         Iterator<Person> persIt = hashSet.iterator();
-        while (persIt.hasNext())    {
+        while (persIt.hasNext() )    {
             int i = 0;
             personArrayList.add(persIt.next());
             System.out.println(personArrayList.get(i));
-            i++;
+            System.out.println(i);
+            i++; // why is i not incrementing??
         }
         int expectedInt = 4;
         int actualInt = hashSet.size();
 
         Assert.assertEquals(expectedInt, actualInt);
-
     }
+
+    @Test
+    public void hashSetTest2() {
+        Person person = new Person("Frank", 1985);
+        Person person1 = new Person("Barney", 1900);
+        Person person2 = new Person("Fred", 1901);
+        Person person3 = new Person("Mac", 2005);
+
+        HashSet<Person> hashSet = new HashSet<Person>();
+        hashSet.add(person);
+        hashSet.add(person1);
+        hashSet.add(person2);
+        hashSet.add(person3);
+
+        hashSet.clear();
+
+        int expectedInt = 0;
+        int actualInt = hashSet.size();
+
+        boolean expectedBool = true;
+        boolean actualBool = hashSet.isEmpty();
+
+        Assert.assertEquals(expectedInt, actualInt);
+        Assert.assertEquals(expectedBool, actualBool);
+    }
+
+
 
 
 
